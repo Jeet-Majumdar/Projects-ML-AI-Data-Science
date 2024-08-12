c00713c345fd68c8f6cd98687915aa3328f9c926
Jeet-Majumdar <mail2majumdar@gmail.com>
2024-08-13 03:16:51 +0530

    update

--- Gen_AI_find_industry_interest_in_IISC_students/scrape_outlook_inbox.py
+++ Gen_AI_find_industry_interest_in_IISC_students/scrape_outlook_inbox.py
@@ -1,3 +1,4 @@
+import re
 import win32com.client as client
 import pandas as pd
 
@@ -17,6 +18,7 @@ with open('recruters.csv', 'w') as f:
         item_subject = m.Subject
         if "IISc || Full time Placement Invite 2024-25" in item_subject:
             company = item_subject.split('||')[-1].replace('\n', '').replace(',', '')
+            company = re.sub('[^0-9a-zA-Z]+', '', company)
             to = str(m.To).replace('\n', '').replace(',', '')
             sender = str(m.SenderEmailType).replace('\n', '').replace(',', '')
             date = m.SentOn.strftime('%d-%m-%Y')
@@ -24,14 +26,25 @@ with open('recruters.csv', 'w') as f:
             try:
                 name = body.split('\n')[0].replace(',', '').replace('To:', '')\
                     .replace(':', '').replace('Hello', '').replace('Dear', '').strip()
+                name = re.sub('[^0-9a-zA-Z]+', '', name)
                 statement = f"{date},{name},{company},{to}"
                 company_emails += f"{to} "
                 f.write(f"{statement}\n")
+                print(statement)
             except:
-                name = ""
-                statement = f"{date},{name},{company},{to}"
-                company_emails += f"{to} "
-                f.write(f"{statement}\n")
+                try:
+                    name = ""
+                    statement = f"{date},{name},{company},{to}"
+                    company_emails += f"{to} "
+                    f.write(f"{statement}\n")
+                    print(statement)
+                except:
+                    name = ""
+                    statement = f",,{company},{to}"
+                    company_emails += f"{to} "
+                    f.write(f"{statement}\n")
+                    print(statement)
+
             
             
 
